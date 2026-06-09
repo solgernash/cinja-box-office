e677114ae4829e85a16eec26843ebd96ba5fb203
Abhinav <abhinav.aravind123@gmail.com>
2026-06-08 22:35:51 -0400

    Initial Java+Maven+SpringBoot Backend Setup

---
+++ src/main/java/com/cinema/CinemaBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaBookingSystemApplication.class, args);
+	}
+
+}
