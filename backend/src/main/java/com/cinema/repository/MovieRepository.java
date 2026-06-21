31fc2ccaca8183b2ec93ef65fd5973886bcd3612
Abhinav <abhinav.aravind123@gmail.com>
2026-06-20 22:10:39 -0400

    Updated MovieRepository.java (repository)

--- backend/src/main/java/com/cinema/repository/MovieRepository.java
+++ backend/src/main/java/com/cinema/repository/MovieRepository.java
@@ -1,4 +1,24 @@
 package com.cinema.repository;
 
-public class MovieRepository {
-}
+import com.cinema.model.Movie;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;    //marks this interface as a spring repository component
+
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, String> {
+
+    /* MongoRepository will automatically provide the implementations at runtime */
+
+    //  Search movies by title
+    List<Movie> findByTitleContainingIgnoreCase(String title);
+
+    // Filter by genre
+    List<Movie> findByGenreIgnoreCase(String genre);
+
+    // Filter by status ( whether a movie is RUNNING / COMING_SOON)
+    List<Movie> findByStatusIgnoreCase(String status);
+
+
+} //end interface
