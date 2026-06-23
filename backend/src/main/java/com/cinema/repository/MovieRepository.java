ce6912722221bfe8c4254f094d32089c330b24f6
Abhinav <abhinav.aravind123@gmail.com>
2026-06-23 19:19:33 -0400

    Added some comments to MovieRepository.java

--- backend/src/main/java/com/cinema/repository/MovieRepository.java
+++ backend/src/main/java/com/cinema/repository/MovieRepository.java
@@ -1,7 +1,7 @@
 package com.cinema.repository;
 
 import com.cinema.model.Movie;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;   //imports the MongoDB interface (MongoRepository) from SpringData MongoDB
 import org.springframework.stereotype.Repository;    //marks this interface as a spring repository component
 
 import java.util.List;
