9b725e6ddda0c128efd070e2433556c3ab86f265
Abhinav <abhinav.aravind123@gmail.com>
2026-07-22 14:51:20 -0400

    PaymentService.java

---
+++ backend/src/main/java/com/cinema/service/PaymentService.java
@@ -0,0 +1,8 @@
+package com.cinema.service;
+
+public class PaymentService {
+
+    //CheckoutFacade.java design pattern already implemented, PaymentService should implement the payment transaction logic that CheckoutFacade will call
+    //simple simulated payment method that returns success/failure I believe is enough for sprint 3/final demo.
+
+}
