1206b5731c42d752544dd1dfa8ec6f09cecc9bd8
Abhinav <abhinav.aravind123@gmail.com>
2026-07-22 14:52:11 -0400

    Facade Design pattern rough draft

--- backend/src/main/java/com/cinema/pattern/CheckoutFacade.java
+++ backend/src/main/java/com/cinema/pattern/CheckoutFacade.java
@@ -1,4 +1,58 @@
-package com.cinema.pattern;
+/* package com.cinema.pattern;
+
+import com.cinema.model.*;
+import com.cinema.service.*;
 
 public class CheckoutFacade {
-}
+
+    private PaymentCardService paymentCardService;
+    private PaymentService paymentService;
+    private BookingService bookingService;
+    private TicketService ticketService;
+    private EmailService emailService;
+
+
+    public CheckoutFacade(
+            PaymentCardService paymentCardService,
+            PaymentService paymentService,
+            BookingService bookingService,
+            TicketService ticketService,
+            EmailService emailService ) {
+        this.paymentCardService = paymentCardService;
+        this.paymentService = paymentService;
+        this.bookingService = bookingService;
+        this.ticketService = ticketService;
+        this.emailService = emailService;
+    }
+
+
+    public Booking checkout(Customer customer, PaymentCard card) {
+
+        // Retrieve/use saved payment card
+        PaymentCard selectedCard = card;
+
+        //  Process payment
+        boolean paymentSuccessful =
+                paymentService.processPayment(selectedCard);
+
+
+        if (!paymentSuccessful) {
+            throw new RuntimeException("Payment failed");
+        }
+
+
+        // Create booking
+        Booking booking = bookingService.createBooking(customer);
+
+
+        //  Generate tickets
+        ticketService.generateTickets(booking);
+
+
+        //  Send confirmation email
+        emailService.sendConfirmationEmail(customer, booking);
+
+
+        return booking;
+    }
+}  */
