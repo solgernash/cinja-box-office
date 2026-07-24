fe461c8a979e10001d9925970df59e2836e4d52c
Abhinav <abhinav.aravind123@gmail.com>
2026-07-24 12:03:38 -0400

    Final Repository Structure

--- backend/src/main/java/com/cinema/repository/ShowSeatRepository.java
+++ backend/src/main/java/com/cinema/repository/ShowSeatRepository.java
@@ -1,8 +1,11 @@
 package com.cinema.repository;
 
+import com.cinema.model.Show;
 import com.cinema.model.ShowSeat;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ShowSeatRepository extends MongoRepository<ShowSeat, String> {
+import java.util.List;
 
+public interface ShowSeatRepository extends MongoRepository<ShowSeat, String> {
+    List<ShowSeat> findByShow(Show show);
 }
